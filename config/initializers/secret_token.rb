471870a1c4248f70cc6ae3939ee3e723752d31b3
John Plummer <john@memphis.co.uk>
2011-01-26 22:02:33 +0000

    Project Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agate::Application.config.secret_token = '19a7e19c86c451524e56ad8a163f88c00949aed87b9430d9eec7a5c2ad297bd79331dc1f6e8e27cd10dd22ab87e2724b9bd6c7233d1ef54b5c492d366d99d107'
